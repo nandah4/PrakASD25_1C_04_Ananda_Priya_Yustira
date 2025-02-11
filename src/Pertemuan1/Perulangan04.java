ec1290f48e2c7b8896f99c04298d3b289dab3547
nandah4 <nyustira6@gmail.com>
2025-02-11 12:30:17 +0700

    P1_Praktikum Perulangan

---
+++ src/Pertemuan1/Perulangan04.java
@@ -0,0 +1,29 @@
+package Pertemuan1;
+
+import java.util.Scanner;
+
+public class Perulangan04 {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.print("Masukkan NIM Anda: ");
+        long nim = sc.nextLong();
+
+        long lastDigit = nim % 100;
+
+        if (lastDigit < 10) {
+            lastDigit += 10;
+        }
+
+        for (int i = 1; i < lastDigit; i++) {
+            if (i == 6 || i == 10) {
+                continue;
+            } else if (i % 2 != 0) {
+                System.out.print("*");
+            } else {
+                System.out.print(i);
+            }
+        }
+
+    }
+}
