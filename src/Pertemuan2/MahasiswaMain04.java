3b1ddcffece3caa6ca0994e4b310ffe7cc37b198
nandah4 <nyustira6@gmail.com>
2025-02-18 09:32:54 +0700

    P2_Percobaan2

---
+++ src/Pertemuan2/MahasiswaMain04.java
@@ -0,0 +1,16 @@
+package Pertemuan2;
+
+public class MahasiswaMain04 {
+    public static void main(String[] args) {
+        Mahasiswa04 mhs1 = new Mahasiswa04();
+        mhs1.nama = "Muhammad Ali Farhan";
+        mhs1.nim = "244107090142";
+        mhs1.kelas = "SI 2J";
+        mhs1.ipk = 3.55;
+
+        mhs1.tampilkanInformasi();
+        mhs1.ubahKelas("SI 2K");
+        mhs1.updateIpk(3.60);
+        mhs1.tampilkanInformasi();
+    }
+}
