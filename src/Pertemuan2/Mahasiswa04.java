33070677dad6f515a0bca5b3b8f45909edbd3ea2
nandah4 <nyustira6@gmail.com>
2025-02-18 09:24:08 +0700

    P2_Percobaan1_Pertanyaan6

--- src/Pertemuan2/Mahasiswa04.java
+++ src/Pertemuan2/Mahasiswa04.java
@@ -16,7 +16,12 @@ public class Mahasiswa04 {
     }
 
     void updateIpk(double ipkBaru) {
-        ipk = ipkBaru;
+        if (ipk >= 0.0 && ipk <= 4.0) {
+            ipk = ipkBaru;
+        } else {
+            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
+        }
+
     }
 
     String nilaiKerja() {
