6d9dc3072caebeb1fd34c5b91bac0884f9640a17
nandah4 <nyustira6@gmail.com>
2025-02-25 09:45:45 +0700

    P3_Percobaan2_Pertanyaan1

--- src/Pertemuan3/Mahasiswa04.java
+++ src/Pertemuan3/Mahasiswa04.java
@@ -5,4 +5,11 @@ public class Mahasiswa04 {
     public String nama;
     public String kelas;
     public float ipk;
+
+    void cetakInfo() {
+        System.out.println("NIM     : " + this.nim);
+        System.out.println("Nama    : " + this.nama);
+        System.out.println("Kelas   : " + this.kelas);
+        System.out.println("IPK     : " + this.ipk);
+    }
 }
--- src/Pertemuan3/MahasiswaDemo04.java
+++ src/Pertemuan3/MahasiswaDemo04.java
@@ -26,10 +26,7 @@ public class MahasiswaDemo04 {
 
         for (int i = 0; i < arrayOfMahasiswa.length; i++) {
             System.out.println("Data Mahasiswa ke-" + (i + 1));
-            System.out.println("NIM     : " + arrayOfMahasiswa[i].nim);
-            System.out.println("Nama    : " + arrayOfMahasiswa[i].nama);
-            System.out.println("Kelas   : " + arrayOfMahasiswa[i].kelas);
-            System.out.println("IPK     : " + arrayOfMahasiswa[i].ipk);
+            arrayOfMahasiswa[i].cetakInfo();
             System.out.println("--------------------------------------------");
         }
 
